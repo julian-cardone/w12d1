b564bcd9b71552ea097d0a4c4395556bfb334a5f
Julian Cardone <juliancardone1@gmail.com>
2022-11-28 14:45:08 -0500

    finished phase 3

--- frontend/src/store/pokemon.js
+++ frontend/src/store/pokemon.js
@@ -1,43 +1,43 @@
-import { LOAD_ITEMS, REMOVE_ITEM, ADD_ITEM } from './items';
+import { LOAD_ITEMS, REMOVE_ITEM, ADD_ITEM } from "./items";
 
-const LOAD = 'pokemon/LOAD';
-const LOAD_TYPES = 'pokemon/LOAD_TYPES';
-const ADD_ONE = 'pokemon/ADD_ONE';
+const LOAD = "pokemon/LOAD";
+const LOAD_TYPES = "pokemon/LOAD_TYPES";
+const ADD_ONE = "pokemon/ADD_ONE";
 
-const load = list => ({
+const load = (list) => ({
   type: LOAD,
-  list
+  list,
 });
 
-const loadTypes = types => ({
+const loadTypes = (types) => ({
   type: LOAD_TYPES,
-  types
+  types,
 });
 //action creator
-const addOnePokemon = pokemon => ({
+const addOnePokemon = (pokemon) => ({
   type: ADD_ONE,
-  pokemon
+  pokemon,
 });
 
-export const getPokemon = () => async dispatch => {
+export const getPokemon = () => async (dispatch) => {
   const response = await fetch(`/api/pokemon`);
-//thunk action 
+  //thunk action
   if (response.ok) {
     const list = await response.json();
     dispatch(load(list));
   }
 };
 
-export const getPokemonById = (id) => async dispatch => {
+export const getPokemonById = (id) => async (dispatch) => {
   const response = await fetch(`/api/pokemon/${id}`);
 
-  if (response.ok){
+  if (response.ok) {
     const pokemon = await response.json();
     dispatch(addOnePokemon(pokemon));
   }
 };
 
-export const getPokemonTypes = () => async dispatch => {
+export const getPokemonTypes = () => async (dispatch) => {
   const response = await fetch(`/api/pokemon/types`);
 
   if (response.ok) {
@@ -46,41 +46,56 @@ export const getPokemonTypes = () => async dispatch => {
   }
 };
 
+export const createPokemon = (pokemon) => async (dispatch) => {
+  const response = await fetch("/api/pokemon", {
+    method: "POST",
+    body: JSON.stringify(pokemon),
+    headers: { "Content-Type": "application/json" }
+  });
+
+  if (response.ok){
+    const pokemon = await response.json();
+    dispatch(addOnePokemon(pokemon));
+  }
+};
+
 const initialState = {
   list: [],
-  types: []
+  types: [],
 };
 
 const sortList = (list) => {
-  return list.sort((pokemonA, pokemonB) => {
-    return pokemonA.number - pokemonB.number;
-  }).map((pokemon) => pokemon.id);
+  return list
+    .sort((pokemonA, pokemonB) => {
+      return pokemonA.number - pokemonB.number;
+    })
+    .map((pokemon) => pokemon.id);
 };
 
 const pokemonReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOAD: 
+    case LOAD:
       const allPokemon = {};
-      action.list.forEach(pokemon => {
+      action.list.forEach((pokemon) => {
         allPokemon[pokemon.id] = pokemon;
       });
       return {
         ...allPokemon,
         ...state,
-        list: sortList(action.list)
+        list: sortList(action.list),
       };
-    case LOAD_TYPES: 
+    case LOAD_TYPES:
       return {
         ...state,
-        types: action.types
+        types: action.types,
       };
-    case ADD_ONE: 
+    case ADD_ONE:
       if (!state[action.pokemon.id]) {
         const newState = {
           ...state,
-          [action.pokemon.id]: action.pokemon
+          [action.pokemon.id]: action.pokemon,
         };
-        const pokemonList = newState.list.map(id => newState[id]);
+        const pokemonList = newState.list.map((id) => newState[id]);
         pokemonList.push(action.pokemon);
         newState.list = sortList(pokemonList);
         return newState;
@@ -89,16 +104,16 @@ const pokemonReducer = (state = initialState, action) => {
         ...state,
         [action.pokemon.id]: {
           ...state[action.pokemon.id],
-          ...action.pokemon
-        }
+          ...action.pokemon,
+        },
       };
-    case LOAD_ITEMS: 
+    case LOAD_ITEMS:
       return {
         ...state,
         [action.pokemonId]: {
           ...state[action.pokemonId],
-          items: action.items.map(item => item.id)
-        }
+          items: action.items.map((item) => item.id),
+        },
       };
     case REMOVE_ITEM:
       return {
@@ -107,8 +122,8 @@ const pokemonReducer = (state = initialState, action) => {
           ...state[action.pokemonId],
           items: state[action.pokemonId].items.filter(
             (itemId) => itemId !== action.itemId
-          )
-        }
+          ),
+        },
       };
     case ADD_ITEM:
       console.log(action.item);
@@ -116,12 +131,12 @@ const pokemonReducer = (state = initialState, action) => {
         ...state,
         [action.item.pokemonId]: {
           ...state[action.item.pokemonId],
-          items: [...state[action.item.pokemonId].items, action.item.id]
-        }
+          items: [...state[action.item.pokemonId].items, action.item.id],
+        },
       };
     default:
       return state;
   }
-}
+};
 
 export default pokemonReducer;
